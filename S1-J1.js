8b4a5b2bd7f2ffe54ecc756ce3df5598b6e165c9
AbdeljalilElouafi <abdeljalilelouafi2@gmail.com>
2025-03-25 23:24:33 +0000

    Create S1-J1.js

---
+++ S1-J1.js
@@ -0,0 +1,19 @@
+let personne = {
+    nom: "mahmouuud",
+    age: 19,
+    ville: "SALé"
+};
+console.log(personne.nom);  
+console.log(personne.age);  
+console.log(personne.ville);
+
+let compteur = 5;
+console.log(compteur); 
+compteur = 10;
+console.log(compteur);
+
+const x = 5;
+console.log(x); 
+x = 10;
+
+// i get this error: "Uncaught TypeError: Assignment to constant variable."
